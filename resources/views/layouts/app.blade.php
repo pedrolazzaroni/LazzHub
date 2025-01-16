dfb21cdb1411f04fcdfb9525315f7aa26bde58f1
pedrolazzaroni <phlazzaroni@gmail.com>
2025-01-16 15:23:38 -0300

    Arrumando svgs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,13 +75,11 @@
                     @else
                         <a href="{{ route('login') }}" class="text-gray-700 hover:text-indigo-600 px-3 py-2">
                             <svg class="h-5 w-5 inline-block mr-1 sm:hidden" fill="currentColor" viewBox="0 0 24 24">
-                                <!-- SVG de login para dispositivos móveis -->
                             </svg>
-                            <span class="hidden sm:inline">Login</span>
+                            <span class="">Login</span>
                         </a>
                         <a href="{{ route('register') }}" class="text-gray-700 hover:text-indigo-600 px-3 py-2">
                             <svg class="h-5 w-5 inline-block mr-1 sm:hidden" fill="currentColor" viewBox="0 0 24 24">
-                                <!-- SVG de registro para dispositivos móveis -->
                             </svg>
                             <span class="hidden sm:inline">Registro</span>
                         </a>
@@ -102,23 +100,27 @@
             </p>
             <p class="text-center text-gray-500 flex items-center justify-center flex-col sm:flex-row">
                 Desenvolvido por Pedro Lazzaroni
-                <div class="flex sm:mt-0 sm:ml-2 py-1 justify-center">
-                    <a href="https://github.com/pedrolazzaroni" class="text-indigo-600 hover:text-indigo-800 mx-1">
-                        <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 24 24">
+                <div class="flex sm:mt-0 sm:ml-2 py-1 justify-center items-center space-x-4">
+                    <!-- GitHub -->
+                    <a href="https://github.com/pedrolazzaroni" class="text-indigo-600 hover:text-indigo-800" target="_blank">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 24 24" fill="currentColor" aria-label="GitHub">
                             <path d="M12 0C5.373 0 0 5.373 0 12c0 5.303 3.438 9.8 8.205 11.387.6.111.82-.261.82-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.757-1.333-1.757-1.089-.744.083-.729.083-.729 1.205.084 1.838 1.237 1.838 1.237 1.07 1.835 2.809 1.305 3.495.998.108-.775.418-1.305.762-1.605-2.665-.305-5.466-1.333-5.466-5.93 0-1.31.467-2.381 1.235-3.221-.123-.303-.535-1.523.117-3.176 0 0 1.008-.322 3.3 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.29-1.552 3.297-1.23 3.297-1.23.654 1.653.242 2.873.12 3.176.77.84 1.233 1.911 1.233 3.221 0 4.61-2.804 5.623-5.475 5.92.43.372.823 1.102.823 2.222v3.293c0 .319.218.694.825.576C20.565 21.796 24 17.299 24 12c0-6.627-5.373-12-12-12z"/>
                         </svg>
                     </a>
-                    <a href="https://www.linkedin.com/in/pedrolazzaroni" class="text-indigo-600 hover:text-indigo-800 mx-1">
-                        <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 24 24">
+                    <!-- LinkedIn -->
+                    <a href="https://www.linkedin.com/in/pedrolazzaroni" class="text-indigo-600 hover:text-indigo-800" target="_blank">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 24 24" fill="currentColor" aria-label="LinkedIn">
                             <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.761 0 5-2.239 5-5v-14c0-2.761-2.239-5-5-5zm-11 19h-3v-10h3v10zm-1.5-11.268c-.966 0-1.75-.784-1.75-1.75s.784-1.75 1.75-1.75 1.75.784 1.75 1.75-.784 1.75-1.75 1.75zm13.5 11.268h-3v-5.5c0-1.378-1.122-2.5-2.5-2.5s-2.5 1.122-2.5 2.5v5.5h-3v-10h3v1.268c.878-.878 2.122-1.268 3.5-1.268 2.481 0 4.5 2.019 4.5 4.5v5.5z"/>
                         </svg>
                     </a>
-                    <a href="https://www.instagram.com/pedro_lazzaroni" class="text-indigo-600 hover:text-indigo-800 mx-1">
-                        <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 24 24">
+                    <!-- Instagram -->
+                    <a href="https://www.instagram.com/pedro_lazzaroni" class="text-indigo-600 hover:text-indigo-800" target="_blank">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 22 22" fill="currentColor" aria-label="Instagram">
                             <path d="M12 2.163c3.204 0 3.584.012 4.85.07 1.366.062 2.633.334 3.608 1.31.975.975 1.248 2.242 1.31 3.608.058 1.266.07 1.646.07 4.85s-.012 3.584-.07 4.85c-.062 1.366-.334 2.633-1.31 3.608-.975.975-2.242 1.248-3.608 1.31-1.266.058-1.646.07-4.85.07s-3.584-.012-4.85-.07c-1.366-.062-2.633-.334-3.608-1.31-.975-.975-1.248-2.242-1.31-3.608-.058-1.266-.07-1.646-.07-4.85s.012-3.584.07-4.85c.062-1.366.334-2.633 1.31-3.608.975-.975 2.242-1.248 3.608-1.31 1.266-.058 1.646-.07 4.85-.07zm0-2.163c-3.259 0-3.667.014-4.947.072-1.281.058-2.563.334-3.537 1.308-.974.974-1.25 2.256-1.308 3.537-.058 1.28-.072 1.688-.072 4.947s.014 3.667.072 4.947c.058 1.281.334 2.563 1.308 3.537.974.974 2.256 1.25 3.537 1.308 1.28.058 1.688.072 4.947.072s3.667-.014 4.947-.072c1.281-.058 2.563-.334 3.537-1.308.974-.974 1.25-2.256 1.308-3.537.058-1.28.072-1.688.072-4.947s-.014-3.667-.072-4.947c-.058-1.281-.334-2.563-1.308-3.537-.974-.974-2.256-1.25-3.537-1.308-1.28-.058-1.688-.072-4.947-.072zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.162 6.162 6.162 6.162-2.759 6.162-6.162-2.759-6.162-6.162-6.162zm0 10.162c-2.207 0-4-1.793-4-4s1.793-4 4-4 4 1.793 4 4-1.793 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.441s.645 1.441 1.441 1.441 1.441-.645 1.441-1.441-.645-1.441-1.441-1.441z"/>
                         </svg>
                     </a>
                 </div>
+
             </p>
         </div>
     </footer>
